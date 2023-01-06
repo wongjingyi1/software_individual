4f6003a9821b2c363596d8ba588b600882d91a70
TAN ZHEN XUN <tanzhenxun1118@e.newera.edu.my>
2023-01-07 00:00:40 +0800

    Student

---
+++ student_dashboard.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Student - Dahboard</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/f9f6f2f33c.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="css/sidebar_nvg.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="border-dark border-bottom py-2">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <div>Dashboard</div>
+            <div class="d-flex justify-content-center align-items-center">
+                <i class="fa-solid fa-bell fa-2x pe-2"></i>
+                <div>Username</div>
+                <img src="images/no_people_img.png" alt="" class="rounded" width="30px">
+            </div>
+        </div>
+    </div>
+    <div class="container ">
+        <div class="text-decoration">Overview</div>
+        <div class="d-flex justify-content-between align-items-between flex-wrap pb-md-0 pb-3 row">
+            <div class="card bg-dark bg-gradient border border-0 hover-blacktogrey mx-lg-3 mx-md-2 col">
+                <div class="card-body text-white d-flex flex-column align-items-center justify-content-between ">
+                    <div class="card-text fs-lg-2">Pending</div>
+                    <h3 class="card-title">3</h3>
+                </div>
+            </div>
+
+            <div class="card bg-secondary bg-gradient border border-0 hover-greytoback mx-lg-3 mx-md-2 col">
+                <div class="card-body text-white d-flex flex-column align-items-center justify-content-between ">
+                    <div class="card-text">Keep In View</div>
+                    <h3 class="card-title">3</h3>
+                </div>
+            </div>
+
+            <div class="card bg-dark bg-gradient border border-0 hover-blacktogrey mx-lg-3 mx-md-2 col">
+                <div class="card-body text-white d-flex flex-column align-items-center justify-content-between ">
+                    <div class="card-text">Active</div>
+                    <h3 class="card-title">3</h3>
+                </div>
+            </div>
+            <div class="card bg-dark bg-gradient border border-0 hover-blacktogrey mx-lg-3 mx-md-2 col">
+                <div class="card-body text-white d-flex flex-column align-items-center justify-content-between ">
+                    <div class="card-text">Closed</div>
+                    <h3 class="card-title">3</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>

b2947759879581c1eff5a7636f4e81a670d3b102
2020066-DWD <wongjunann0227@e.newera.edu.my>
2023-01-11 01:46:27 +0800

    create footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <script>document.write(new Date().getFullYear())</script>&nbsp;<strong><span>All-Star Team</span></strong>. All Rights Reserved
+    </div>
+  </footer><!-- End Footer -->
--- helpdesk_complaint_management.php
+++ helpdesk_complaint_management.php
@@ -227,6 +227,9 @@ include "reusable_components/user_session.php"
             ?>
         </section>
     </main>
+
+    <?php include "footer.php" ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
     <script src="js/main.js"></script>

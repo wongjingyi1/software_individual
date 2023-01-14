18d423bfcb155d52ec476888ff28cbf14928989f
Sia Chee Kang <siacheekang0118@e.newera.edu.my>
2023-01-14 11:52:46 +0800

    change color

--- profile.php
+++ profile.php
@@ -267,8 +267,8 @@ include "reusable_components/user_session.php"
                         <input type="password" class="form-control" name="con_pass" id="com_pass">
                     </div>
                     <div class="d-flex mt-5">
-                        <button type="submit" class="btn btn-secondary col-4">Update</button>
-                        <button type="button" class="btn btn-secondary ms-3 col-4" onclick="window.location.href = 'dashboard.php'">Cancel</button>
+                        <button type="submit" class="btn btn-success col-4">Update</button>
+                        <button type="button" class="btn btn-danger ms-3 col-4" onclick="window.location.href = 'dashboard.php'">Cancel</button>
                     </div>
 
                 </div>
